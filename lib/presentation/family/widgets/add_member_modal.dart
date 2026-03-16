5716fd208db46302a5388b3869e444e83a9316a7
Btbach25 <btbach25@gmail.com>
2026-03-16 15:32:30 +0700

    Fix: Fix conflict version.

--- lib/presentation/family/widgets/add_member_modal.dart
+++ lib/presentation/family/widgets/add_member_modal.dart
@@ -169,7 +169,7 @@ class _AddMemberModalState extends State<AddMemberModal>
                 ),
                 const SizedBox(height: 8),
                 DropdownButtonFormField<RelationshipType>(
-                  value: _selectedRelationship,
+                  initialValue: _selectedRelationship,
                   decoration: const InputDecoration(
                     hintText: 'Chọn mối quan hệ',
                   ),
